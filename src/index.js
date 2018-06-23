b95a37da3666a227533a176580a1aaeca3196cdc
Bian2017 <bgl_cumt_zju@163.com>
2018-06-23 23:18:32 +0800

    feat: 将Redux和React结合起来使用

--- src/index.js
+++ src/index.js
@@ -1,36 +1,16 @@
-
+import React from 'react'
+import ReactDom from 'react-dom'
 import { createStore } from 'redux'
-
-function counter(state, action) {
-  switch (action.type) {
-    case 'add':
-      return state + 1
-    case 'reduce':
-      return state - 1
-    default:
-      return 10
-  }
-}
-
+import App from './App'
+import { counter } from './reduxStore';
 
 const store = createStore(counter)
 
-const init = store.getState()
-
-console.log(init)
-
-function listener() {
-  const current = store.getState()
-  console.log('Current count', current)
+function render() {
+  ReactDom.render(<App store={store} />, document.getElementById('root'))
 }
 
-store.subscribe(listener)
+render()
 
-store.dispatch({
-  type: 'add'
-})
-
-store.dispatch({
-  type: 'add'
-})
-
+// 当store发生变化时，调用render
+store.subscribe(render)
