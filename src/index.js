98e14d0a4c0eececc0e5d8c3d2ffd3c9f10d6c5b
Bian2017 <bgl_cumt_zju@163.com>
2018-06-23 22:49:46 +0800

    feat: 添加Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { createStore } from 'redux'
+
+function counter(state, action) {
+  switch (action.type) {
+    case 'add':
+      return state + 1
+    case 'reduce':
+      return state - 1
+    default:
+      return 10
+  }
+}
+
+
+const store = createStore(counter)
+
+const init = store.getState()
+
+console.log(init)
+
+function listener() {
+  const current = store.getState()
+  console.log('Current count', current)
+}
+
+store.subscribe(listener)
+
+store.dispatch({
+  type: 'add'
+})
+
+store.dispatch({
+  type: 'add'
+})
+
