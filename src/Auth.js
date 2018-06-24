196d247bda8395bc28f2204460235cf5e453abe3
wb-bgl354174 <wb-bgl354174@alibaba-inc.com>
2018-06-24 17:35:48 +0800

    feat:在Redux实现后端请求

--- src/Auth.js
+++ src/Auth.js
@@ -1,35 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { login } from './Auth.redux'
+import { login, getUserData } from './Auth.redux'
 import { Redirect } from 'react-router-dom'
-import axios  from 'axios'
 
 @connect(
   state => state.auth,
-  { login }
+  { login, getUserData }
 )
 
 class Auth extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: {}
-    }
-  }
-  
   componentDidMount() {
-    axios.get('/data')
-      .then(res => {
-        if(res.status == 200) {
-          this.setState({data: res.data})
-        }
-      })
+    this.props.getUserData()
   }
 
   render() {
     return (
       <div>
-        <h2>我的名字是{this.state.data.user}</h2>
+        <h2>我的名字是{this.props.user}， 年龄是{this.props.age}</h2>
         {this.props.isAuth ? <Redirect to='/dashboard' /> : null}
         <h2>请先登录</h2>
         <button onClick={this.props.login}>登录</button>
