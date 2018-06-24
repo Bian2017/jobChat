b20c7d26cd9a948e18c07506444cc41809cc0bbb
wb-bgl354174 <wb-bgl354174@alibaba-inc.com>
2018-06-24 18:20:20 +0800

    feat:删除测试文件

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
-import Auth from './Auth'
-import Dashboard  from './Dashboard'
 import reducers from './reducer'
 import './config'
 
@@ -20,11 +18,6 @@ const store = createStore(reducers, compose(
 ReactDom.render(
   (<Provider store={store}>
     <BrowserRouter>
-      <Switch>
-        <Route path='/login' exact component={Auth}></Route>
-        <Route path='/dashboard' component={Dashboard}></Route>
-        <Redirect to='/dashboard'></Redirect>
-      </Switch>
     </BrowserRouter>
   </Provider >),
   document.getElementById('root')
--- src/reducer.js
+++ src/reducer.js
@@ -2,8 +2,6 @@
   两个reducers，每个reducers都有一个state。此时需要合并reducers
 */
 import { combineReducers } from 'redux'       // 合并所有reducer 并且返回
-import { counter } from './index.redux'
-import { auth } from './Auth.redux'
 
-export default combineReducers({ counter, auth })
+export default combineReducers({  })
 
