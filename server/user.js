9689224e6ef5a5fb746588f7c66ef249d54ad9e2
wb-bgl354174 <wb-bgl354174@alibaba-inc.com>
2018-06-25 13:35:05 +0800

    feat:完成密码加密实现

--- server/user.js
+++ server/user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const utils = require('utility')
 const Router = express.Router()
 const model = require('./model')
 
@@ -6,17 +7,18 @@ const User = model.getModel('user')
 
 Router.get('/list', function (req, res) {
   User.find({}, function (err, doc) {
-    return res.json()
+    return res.json(doc)
   })
 })
 
 Router.post('/register', function (req, res) {
   const { user, pwd, type } = req.body
+
   User.findOne({ user: user }, function (err, doc) {
     if (doc) {
       return res.json({ code: 1, msg: '用户已存在' })
     }
-    User.create({ user, pwd, type }, function (e, d) {
+    User.create({ user, type, pwd: md5Pwd(pwd) }, function (e, d) {
       if (e) {
         return res.json({ code: 1, msg: '后端出错了' })
       }
@@ -28,5 +30,11 @@ Router.post('/register', function (req, res) {
 Router.get('/info', function (req, res) {
   return res.json({ code: 1 })
 })
+
+// 采用两层md5+盐的方式提高密码复杂度
+function md5Pwd(pwd) {
+  const salt = 'job_boss_chat@w123edc#'
+  return utils.md5(utils.md5(pwd + salt))
+}
 
 module.exports = Router
--- src/container/register/register.js
+++ src/container/register/register.js
@@ -3,6 +3,7 @@ import Logo from '../../component/logo/logo'
 import { List, InputItem, Radio, WingBlank, WhiteSpace, Button, Toast } from 'antd-mobile'
 import { connect } from 'react-redux'
 import { register } from '../../redux/user.redux'
+import { Redirect } from 'react-router-dom'
 
 @connect(
   state => state.user,
@@ -37,6 +38,7 @@ class Register extends React.Component {
 
     return (
       <div>
+        {this.props.redirectTo ? <Redirect to={this.props.redirectTo} /> : null}
         <Logo />
         <List>
           {this.props.msg ? Toast.info(this.props.msg) : null}
