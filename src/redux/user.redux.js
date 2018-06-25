330dce728d5cfaf88f2705a0ba3e2b89018debb1
wb-bgl354174 <wb-bgl354174@alibaba-inc.com>
2018-06-25 14:05:10 +0800

    feat:完成登录功能的前后端实现

--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { getRedirectPath } from '../util/util'
 
 const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
+const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 const ERROR_MSG = 'ERROR_MSG'
 
 const initState = {
@@ -24,6 +25,14 @@ export function user(state = initState, action) {
         isAuth: true,
         ...action.payload
       }
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        msg: '',
+        redirectTo: getRedirectPath(action.payload),
+        isAuth: true,
+        ...action.payload
+      }
     case ERROR_MSG:
       return {
         ...state,
@@ -37,6 +46,10 @@ export function user(state = initState, action) {
 
 function registerSuccess(data) {
   return { type: REGISTER_SUCCESS, payload: data }
+}
+
+function loginSuccess(data) {
+  return { type: LOGIN_SUCCESS, payload: data }
 }
 
 function errorMsg(msg) {
@@ -58,6 +71,23 @@ export function register({ user, pwd, repeatPwd, type }) {
       .then(res => {
         if (res.status === 200 && res.data.code === 0) {
           dispatch(registerSuccess({ user, pwd, type }))
+        } else {
+          dispatch(errorMsg(res.data.msg))
+        }
+      })
+  }
+}
+
+export function login({ user, pwd }) {
+  if (!user || !pwd) {
+    return errorMsg('必须填写用户名和密码')
+  }
+
+  return dispatch => {
+    axios.post('/user/login', { user, pwd })
+      .then(res => {
+        if (res.status === 200 && res.data.code === 0) {
+          dispatch(loginSuccess(res.data.data))
         } else {
           dispatch(errorMsg(res.data.msg))
         }
