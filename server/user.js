aa32d876abf4ce98435a06ff4fcd3446b1cbc8e3
wb-bgl354174 <wb-bgl354174@alibaba-inc.com>
2018-06-25 12:52:34 +0800

    feat:实现express注册功能

--- server/user.js
+++ server/user.js
@@ -4,9 +4,24 @@ const model = require('./model')
 
 const User = model.getModel('user')
 
-Router.get('/list', function(req, res){
-  User.find({}, function(err, doc){
+Router.get('/list', function (req, res) {
+  User.find({}, function (err, doc) {
     return res.json()
+  })
+})
+
+Router.post('/register', function (req, res) {
+  const { user, pwd, type } = req.body
+  User.findOne({ user: user }, function (err, doc) {
+    if (doc) {
+      return res.json({ code: 1, msg: '用户已存在' })
+    }
+    User.create({ user, pwd, type }, function (e, d) {
+      if (e) {
+        return res.json({ code: 1, msg: '后端出错了' })
+      }
+      return res.json({ code: 0 })
+    })
   })
 })
 
