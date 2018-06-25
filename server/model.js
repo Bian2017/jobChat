0b6154b3dfbd76cddcd6223b537060345e2c1572
wb-bgl354174 <wb-bgl354174@alibaba-inc.com>
2018-06-25 12:35:18 +0800

    feat:建立数据库模型

--- server/model.js
+++ server/model.js
@@ -1,9 +1,31 @@
 const mongoose = require('mongoose')
 
-// 链接mongo并且使用chat集合
-const DB_URL = 'mongodb://127.0.0.1:27017/chat'
-
+// 链接mongo并且使用jobChat集合
+const DB_URL = 'mongodb://127.0.0.1:27017/jobChat'
 mongoose.connect(DB_URL)
-mongoose.connection.on('connected', () => {
-  console.log('mongo connect success')
-})
+
+const models = {
+  user: {
+    'user': { type: String, require: true },
+    'pwd': { type: String, require: true },
+    'type': { type: String, require: true },
+    'avatar': { type: String },
+    'desc': { type: String },         // 个人简介
+    'title': { type: String },        // 职位名
+    'company': { type: String },
+    'money': { type: String }
+  },
+  chat: {
+
+  }
+}
+
+for (let i in models) {
+  mongoose.model(i, new mongoose.Schema(models[i]))
+}
+
+module.exports = {
+  getModel: function(name) {
+    return mongoose.model(name)
+  }
+}
