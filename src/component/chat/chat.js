2a7c8311e115c31a1e34faf85cda672511c1160b
wb-bgl354174 <wb-bgl354174@alibaba-inc.com>
2018-06-26 16:58:54 +0800

    feat:完成emoji标签添加

--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavBar, List, InputItem, Icon } from 'antd-mobile'
+import { NavBar, List, InputItem, Icon, Grid } from 'antd-mobile'
 import { connect } from 'react-redux'
 import { getMsgList, sendMsg, recvMsg } from '../../redux/chat.redux'
 import { getChatId } from '../../util/util';
@@ -24,20 +24,34 @@ class Chat extends React.Component {
     }
   }
 
+  fixCarousel() {
+    // 解决emoji的bug
+    setTimeout(function () {
+      window.dispatchEvent(new Event('resize'))
+    }, 0)
+  }
+
   handleSubmit() {
     const from = this.props.user._id
     const to = this.props.match.params.user
     const msg = this.state.text
     this.props.sendMsg({ from, to, msg })
-    this.setState({ text: '' })
+    this.setState({
+      text: '',
+      showEmoji: false
+    })
   }
 
   render() {
+    const emoji = '😀 😃 😄 😁 😆 😅 😂 🤣 😊 😇 😉 😌 😍 😘 😋 😝 😜 🤪 🤨 🧐 🤓 😎 😏 😞 😔 😟 😕 😫 😢 😭 😤 😡 😱 😨 😰 😓 🤗 🤭 🙄 😯 😧 😲 😴 😪 😵 🤐 🤢 🤮 🤧 😷 🤒 👌 👈 👉 🤠 😈 👿 👹 👺 🤡 🖐 😫 😩 😢 😭 😤 😠 😡 💩 👻 💀 ☠️ 💍 💄 💋'
+      .split(' ')
+      .filter(v => v)    //防止多个空格出现
+      .map(v => ({ text: v }))
     const userid = this.props.match.params.user
     const Item = List.Item
     const users = this.props.chat.users
     const chatId = getChatId(userid, this.props.user._id)
-    const chatMsg = this.props.chat.chatmsg.filter(v=>v.chatid===chatId)
+    const chatMsg = this.props.chat.chatmsg.filter(v => v.chatid === chatId)
 
     if (!users[userid]) {
       return null
@@ -77,11 +91,31 @@ class Chat extends React.Component {
               placeholder="请输入"
               value={this.state.text}
               onChange={v => { this.setState({ text: v }) }}
-              extra={<span onClick={() => this.handleSubmit()}>发送</span>}
+              extra={<div>
+                <span style={{ marginRight: 15 }}
+                  onClick={() => {
+                    this.setState({ showEmoji: !this.state.showEmoji })
+                    this.fixCarousel()
+                  }}
+                >😀</span>
+                <span onClick={() => this.handleSubmit()}>发送</span>
+              </div>}
             >
               信息
           </InputItem>
           </List>
+          {this.state.showEmoji ?
+            <Grid
+              data={emoji}
+              columnNum={9}
+              carouselMaxRow={4}
+              isCarousel={true}
+              onClick={el=>{
+                this.setState({
+                  text: this.state.text + el.text
+                })
+              }}
+            /> : null}
         </div>
       </div>
     )
