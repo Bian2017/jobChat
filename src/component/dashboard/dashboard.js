e988633d728c645068aa8cfecb2ee1a8c8f7367f
wb-bgl354174 <wb-bgl354174@alibaba-inc.com>
2018-06-26 16:22:13 +0800

    feat:显示正确的未读数量

--- src/component/dashboard/dashboard.js
+++ src/component/dashboard/dashboard.js
@@ -17,8 +17,10 @@ function Msg() {
 )
 class Dashboard extends React.Component {
   componentDidMount() {
-    this.props.getMsgList()
-    this.props.recvMsg()
+    if(!this.props.chat.chatmsg.length) {
+      this.props.getMsgList()
+      this.props.recvMsg()
+    }
   }
 
   render() {
