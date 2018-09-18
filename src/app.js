22e357f872f926aa80e9d22a0e24c30d2b71ba3c
Bian2017 <bgl_cumt_zju@163.com>
2018-09-18 22:58:29 +0800

    feat:基于路由分割代码

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-
-import Login from './container/login/login'
-import Register from './container/register/register'
-import BossInfo from './container/bossinfo/bossinfo'
-import JobSeekersInfo from './container/jobseekersinfo/jobSeekersInfo'
+import asyncComponent from './component/AsyncComponent'
 import Dashboard from './component/dashboard/dashboard'
 import AuthRoute from './component/authRoute/authRoute'
 import Chat from './component/chat/chat'
+
+const AsyncLogin = asyncComponent(() => import('./container/login/login'))
+const AsyncRegister = asyncComponent(()=>import('./container/register/register'))
+const AsyncBossInfo = asyncComponent(()=>import('./container/bossinfo/bossinfo'))
+const AsyncJobSeekersInfo = asyncComponent(()=>import('./container/jobseekersinfo/jobSeekersInfo'))
 
 class App extends React.Component {
   constructor(props) {
@@ -28,10 +29,10 @@ class App extends React.Component {
     return this.state.isError ? <img src={require('./404.jpg')} alt="error" /> : (<div>
       <AuthRoute></AuthRoute>
       <Switch>
-        <Route path='/bossinfo' component={BossInfo}></Route>
-        <Route path='/jobseekersinfo' component={JobSeekersInfo}></Route>
-        <Route path='/login' component={Login}></Route>
-        <Route path='/register' component={Register}></Route>
+        <Route path='/bossinfo' component={AsyncBossInfo}></Route>
+        <Route path='/jobseekersinfo' component={AsyncJobSeekersInfo}></Route>
+        <Route path='/login' component={AsyncLogin}></Route>
+        <Route path='/register' component={AsyncRegister}></Route>
         <Route path='/chat/:user' component={Chat}></Route>
         <Route component={Dashboard}></Route>
       </Switch>
