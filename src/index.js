e98b4a65d7536f950f373401976b35f9363524da
aisi <bianguilong@mail.i-i.ai>
2019-09-10 19:09:12 +0800

    feat:测试jenkins

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import App from './app'
 import reducers from './reducer'
+
 import './config'
 import './index.css'
 
