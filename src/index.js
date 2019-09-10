7679707445c0250714a1ad48d15d3014d0a381be
aisi <bianguilong@mail.i-i.ai>
2019-09-10 19:02:30 +0800

    feat:测试持续集成

--- src/index.js
+++ src/index.js
@@ -10,8 +10,7 @@ import './config'
 import './index.css'
 
 // 不存在则执行空函数
-const reduxDevtools = window.devToolsExtension ? window.devToolsExtension() : () => { }
-
+const reduxDevtools = window.devToolsExtension ? window.devToolsExtension() : () => {}
 
 // Only chrome can handle the redux dev tool
 // redux compose cannot handle a null or undefined middleware
@@ -19,21 +18,15 @@ if (window.navigator.userAgent.includes('Chrome')) {
   var store = createStore(
     reducers,
     compose(
-      applyMiddleware(thunk),
+      applyMiddleware(thunk)
       // 部署到线上，需注释这行代码，因为在IE和移动端APP该判断条件都为真，
       // 所以正式部署不可以包含这行代码
       // reduxDevtools
     )
   )
 } else {
-  var store = createStore(
-    reducers,
-    compose(
-      applyMiddleware(thunk)
-    )
-  )
+  var store = createStore(reducers, compose(applyMiddleware(thunk)))
 }
-
 
 // const store = createStore(reducers, compose(
 //   applyMiddleware(thunk),
@@ -41,10 +34,10 @@ if (window.navigator.userAgent.includes('Chrome')) {
 // ))
 
 ReactDom.render(
-  (<Provider store={store}>
+  <Provider store={store}>
     <BrowserRouter>
       <App></App>
     </BrowserRouter>
-  </Provider >),
+  </Provider>,
   document.getElementById('root')
 )
